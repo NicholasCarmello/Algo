f548837fe72100ee70b2709a45a5c2109dc70242
Matthew Adam Johnson <Matt.Johnson@Marist.edu>
2021-09-28 21:07:44 -0400

    Refactor test suite.

--- src/HOPL/Types.hs
+++ src/HOPL/Types.hs
@@ -8,9 +8,13 @@
  -}
 module HOPL.Types where
 
+import Text.ParserCombinators.Parsec.Error (ParseError)
+
 -- Represent identifiers using a Haskell String
 type Id = String
 
 -- Source code is simply a text string
 type Source = String
 
+-- Interpreter takes source code and produces a value (of some type)
+type Interpreter a = Source -> Either ParseError a
